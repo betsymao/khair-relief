b38c8906bd11a69f8e0412b83af58fd47413d600
Betsy <132311670+betsymao@users.noreply.github.com>
2024-07-01 20:49:38 +1000

    updated header and styling

---
+++ page-templates/template-home.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Template Name: Template Home
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+
+if ( is_front_page() ) {
+	get_template_part( 'global-templates/hero' );
+}
+
+$wrapper_id = 'full-width-page-wrapper';
+if ( is_page_template( 'page-templates/no-title.php' ) ) {
+	$wrapper_id = 'no-title-page-wrapper';
+}
+?>
+
+<div class="hero">
+    <div class="container">
+        <div class="hero__body d-flex gap-3 flex-column align-items-center justify-content-center">
+            <h1 class="hero__heading display-1 text-center fw-bold">Khair Relief</h1>
+            <div class="hero__subheading text-center">
+                <p class="display-6">Empowering lives, transforming communities</p>
+            </div>
+            <div class="d-flex gap-4 align-items-center">
+                <a class="btn btn-primary btn-lg hero__cta d-flex" href="#" role="button">Donate
+                    <!-- <span class="hero__cta-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
+                        </svg>
+                    </span> -->
+                </a>
+                <a href="#" class="hero__link fs-5">Learn more 
+                    <span class="hero__link-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="wrapper" id="<?php echo $wrapper_id; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- ok. ?>">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area" id="primary">
+
+				<main class="site-main" id="main" role="main">
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, quaerat.</p>
+
+				</main>
+
+			</div><!-- #primary -->
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #<?php echo $wrapper_id; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- ok. ?> -->
+
+<?php
+get_footer();
